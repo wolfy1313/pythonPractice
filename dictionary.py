874d1ed2b795f91260dc891387e4c6bc0d22b26e
jeremyHarrell <hillvulf@gmail.com>
2024-03-04 10:10:19 -0600

    python dictionary lesson

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+acronyms = {
+  'LOL': 'laugh out loud',
+  'IDK': "I don't know",
+  'TBH': "To be honest"
+}
+
+acronyms['BF'] = 'buttfucker'
+
+print(acronyms)
+
+acronyms['TBH'] = 'honestly'
+
+print(acronyms)
