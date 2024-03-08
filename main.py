653645a24e478d4065685d1c4d0367478c32fed5
jeremyHarrell <hillvulf@gmail.com>
2024-03-08 13:51:12 -0600

    a little python practice from pluralsight modules

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 msg = "Roll a dice"
 print(msg)
+
+length = 10
+width = 20
+area = length * width
+print("area is", area)
---
+++ tax.py
@@ -0,0 +1,10 @@
+amount = 154100
+tax = 0.06
+total = amount + amount*tax
+print(total)
+
+hello = "Hello"
+
+name = input("What's your name?\n")
+greeting = hello + ", " + name + "!"
+print(greeting) 
