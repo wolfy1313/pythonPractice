0adfdd4db40a2ee40c6a8595ab5b8e63820664c0
jeremyHarrell <hillvulf@gmail.com>
2024-03-08 14:21:50 -0600

    a little python practice from pluralsight modules

---
+++ decades.py
@@ -0,0 +1,5 @@
+age = int(input("How old is your bitch ass? \n"))
+
+decades = age//10
+years = age % 10
+print("Your little bitch ass is " + str(decades) + " decades and " + str(years) + " years old.")
