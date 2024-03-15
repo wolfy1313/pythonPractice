7e3c0143f61670a1cff1c49c9aae1553a032d9f3
jeremyHarrell <hillvulf@gmail.com>
2024-03-15 10:45:59 -0500

    complete loan calculator python lesson

--- loan.py
+++ loan.py
@@ -10,7 +10,13 @@ monthly_rate = apr/100/12
 for i in range(months):
   interest_paid = money_owed*monthly_rate
   money_owed = money_owed + interest_paid
-  money_owed = money_owed - payment
 
-  print('paid', payment, 'of which', interest_paid, 'was interest', end=" ")
-  print('now you owe', money_owed)
+  if (money_owed - payment <0):
+    print('the last payment is', money_owed)
+    print('you paid of the loan in', (i+1), 'months')
+    break
+
+  money_owed = money_owed - payment
+  
+  print('paid', payment, 'of which', round(interest_paid, 2), 'was interest', end=" ")
+  print('now you owe', round(money_owed, 2))
