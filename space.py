8c5a031d5cbc383e961c7222f584862338ca161e
jeremyHarrell <hillvulf@gmail.com>
2024-03-26 16:06:50 -0500

    more practice with python API

--- space.py
+++ space.py
@@ -4,4 +4,7 @@ response = requests.get('http://api.open-notify.org/astros.json')
 
 json = response.json()
 
-print(json)
+print("da people in space rn are: ")
+
+for person in json['people']:
+  print(person['name'])
