793044c9bf2fd536a73b17103d966d933383af81
jeremyHarrell <hillvulf@gmail.com>
2024-03-26 18:01:21 -0500

    further down the li'l road

--- function.py
+++ function.py
@@ -9,9 +9,10 @@ greeting(name2)
 
 def addition(a,b):
   return a+b
+def main():
+  num1 = int(input('enter your 1st number:\n'))
+  num2 = int(input('enter your 2nd number:\n'))
 
-num1 = int(input('enter your 1st number:\n'))
-num2 = int(input('enter your 2nd number:\n'))
-
-result = addition(num1,num2)
-print('the result is:', result)
+  result = addition(num1,num2)
+  print('the result is:', result)
+main()
