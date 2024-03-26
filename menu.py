b05baad2db23f2a5c5a1e62e586ecb38285c3fce
jeremyHarrell <hillvulf@gmail.com>
2024-03-26 10:57:27 -0500

    dictionary practice

---
+++ menu.py
@@ -0,0 +1,8 @@
+menus = [['egg', 'bacon', 'bagel', 'coffee'],['sandwich', 'soup', 'pickle juice'], ['salad', 'spaghetti', 'taco']]
+
+betterMenus = {'Breakfast': ['egg', 'bacon', 'bagel', 'coffee'],
+               'Lunch': ['blt', 'pb&j', 'turkey sanwich'],
+               'Dinner': ['soup', 'salad', 'spaghetti']}
+
+print(menus[0])
+print(betterMenus['Lunch'])
