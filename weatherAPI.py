718999ba9d68d7c1cdd0c9ff2b88324259663120
jeremyHarrell <hillvulf@gmail.com>
2024-03-26 17:49:16 -0500

    weather API python lesson complete

--- weatherAPI.py
+++ weatherAPI.py
@@ -12,7 +12,7 @@ temp = weather_json.get('current').get('temp_f')
 condition = weather_json.get('current').get('condition').get('text')
 location = weather_json.get('location').get('name')
 
-print("location: ", location, "temperature: ", temp, "conditions: ", condition)
+print("Today's weather in", city, "is", condition.lower(), "and", temp, "degrees.")
 
 # for temp, condition in weather_json['current']:
 #   print(temp['temp_f'], condition['condition'] )
