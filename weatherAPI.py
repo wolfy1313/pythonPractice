ffda411d6951ff0b16cf7b3d00ac420269ce9c0a
jeremyHarrell <hillvulf@gmail.com>
2024-03-26 17:22:43 -0500

    weather API python practice

---
+++ weatherAPI.py
@@ -0,0 +1,18 @@
+import requests
+
+city = 'London'
+
+url = 'http://api.weatherapi.com/v1/current.json?key=d1fc8702867944bbbc4213006242603&q='+city+'&aqi=no'
+
+response = requests.get(url)
+
+weather_json = response.json()
+
+temp = weather_json.get('current').get('temp_f')
+condition = weather_json.get('current').get('condition').get('text')
+location = weather_json.get('location').get('name')
+
+print("location: ", location, "temperature: ", temp, "conditions: ", condition)
+
+# for temp, condition in weather_json['current']:
+#   print(temp['temp_f'], condition['condition'] )
